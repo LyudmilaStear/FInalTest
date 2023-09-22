e5fc4e28d61a99c23f5cbc57d1f613ac9b6fd515
LyudmilaStear <138987986+LyudmilaStear@users.noreply.github.com>
2023-09-22 16:14:27 +0300

    Написали методы для создания массива из строк

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string[] EnterNewArray(int size) //, int minValue, int maxValue)
+            {
+                
+                string[] array = new string[size]; 
+                for (int i = 0; i < size; i++) 
+                {
+                    Console.WriteLine($"Введите элемент массива под индексом {i}");
+                    array[i] = Console.ReadLine();     
+                }                           
+                return array;
+            }
+
+            void WriteArray(string[] array)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write($"'{array[i]}', ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Введите размер массива: ");
+            int size1 = Convert.ToInt32(Console.ReadLine());
+            string[] array = EnterNewArray(size1);
+            Console.WriteLine("Ваш массив: ");
+            WriteArray(array);
